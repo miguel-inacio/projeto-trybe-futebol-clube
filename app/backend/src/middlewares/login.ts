c45259ed8eb907544ec20434fb6884b2f14d4d6b
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-01-23 19:42:30 -0300

    Adiciona validações específicas para composição de senha e email

--- app/backend/src/middlewares/login.ts
+++ app/backend/src/middlewares/login.ts
@@ -2,9 +2,23 @@ import { NextFunction, Request, Response } from 'express';
 
 const validateLoginBody = (req: Request, res: Response, next: NextFunction) => {
   const { email, password } = req.body;
-  if (!email || !password) res.status(400).send('Campos inválidos!');
 
-  return next();
+  if (!email || !password) {
+    return res.status(400).send({ message: 'All fields must be filled' });
+  }
+
+  next();
 };
 
-export default validateLoginBody;
+const validateData = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(email);
+
+  if (!emailRegex || password.length < 6) {
+    return res.status(401).send({ message: 'Incorrect email or password' });
+  }
+
+  next();
+};
+
+export { validateLoginBody, validateData };
--- app/backend/src/routes/login.ts
+++ app/backend/src/routes/login.ts
@@ -1,11 +1,11 @@
 import express = require('express');
-import validateLoginBody from '../middlewares/login';
+import { validateLoginBody, validateData } from '../middlewares/login';
 import LoginController from '../controllers/login';
 
 const loginController = new LoginController();
 
 const router = express.Router();
 
-router.post('/', validateLoginBody, loginController.userLogin.bind(loginController));
+router.post('/', validateLoginBody, validateData, loginController.userLogin.bind(loginController));
 
 export default router;
