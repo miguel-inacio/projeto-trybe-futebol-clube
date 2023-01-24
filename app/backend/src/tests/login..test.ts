f98b96b64252a7948f55ea9340335e14011e3d73
Miguel Inácio <miguel.angelo.inacio@gmail.com>
2023-01-24 11:14:31 -0300

    Adiciona testes de integração para verificação de status de erros na rota de login

--- app/backend/src/tests/login..test.ts
+++ app/backend/src/tests/login..test.ts
@@ -12,34 +12,77 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
+describe('Rota de login', () => {
+  const userData = {
+    email: 'user@user.com',
+    password: 'secret_user'
+  }
 
-  // let chaiHttpResponse: Response;
-
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('retorna erro 400 ao receber email vazio', async () => {
+    const result = await chai.request(app).post('/login').send({ ...userData, email: undefined})
+    expect(result.status).to.be.equal(400);
+    expect(result.body.message).to.be.equal('All fields must be filled');
   });
+
+  it('retorna erro 400 ao receber senha vazia', async () => {
+    const result = await chai.request(app).post('/login').send({ ...userData, password: undefined})
+    expect(result.status).to.be.equal(400);
+    expect(result.body.message).to.be.equal('All fields must be filled');
+  });
+
+  it('retorna erro 401 ao receber email inexistente no banco de dados', async () => {
+    const result = await chai.request(app).post('/login').send({ ...userData, email: 'email@email.com'})
+    expect(result.status).to.be.equal(401);
+    expect(result.body.message).to.be.equal('Incorrect email or password');
+  });
+
+  it('retorna erro 401 ao receber senha incorreta', async () => {
+    const result = await chai.request(app).post('/login').send({ ...userData, password: 'undefined'})
+    expect(result.status).to.be.equal(401);
+    expect(result.body.message).to.be.equal('Incorrect email or password');
+  });
+
+  // it('retorna status 200 ao receber dados válidos', async () => {
+  //   // sinon.stub()
+  //   const result = await chai.request(app).post('/login').send({ ...userData })
+  //   expect(result.status).to.be.equal(200);
+  //   // expect(result.).to.be.equal('ok');
+  // });
+
 });
+
+// describe('Rota de validação em login', () => {
+//   const authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjU0NTI3MTg5fQ.XS_9AA82iNoiVaASi0NtJpqOQ_gHSHhxrpIdigiT-fc';
+
+//   it('retorna erro 401 ao receber token inválido', async () => {
+//     const result = await (await chai.request(app).get('/login')).header('1')
+//     expect(result.status).to.be.equal(401);
+//     expect(result.body.message).to.be.equal('Unauthorized user');
+//   });
+
+  // it('retorna erro 400 ao receber senha vazia', async () => {
+  //   const result = await chai.request(app).post('/login').send({ ...userData, password: undefined})
+  //   expect(result.status).to.be.equal(400);
+  //   expect(result.body.message).to.be.equal('All fields must be filled');
+  // });
+
+  // it('retorna erro 401 ao receber email inexistente no banco de dados', async () => {
+  //   const result = await chai.request(app).post('/login').send({ ...userData, email: 'email@email.com'})
+  //   expect(result.status).to.be.equal(401);
+  //   expect(result.body.message).to.be.equal('Incorrect email or password');
+  // });
+
+  // it('retorna erro 401 ao receber senha incorreta', async () => {
+  //   const result = await chai.request(app).post('/login').send({ ...userData, password: 'undefined'})
+  //   expect(result.status).to.be.equal(401);
+  //   expect(result.body.message).to.be.equal('Incorrect email or password');
+  // });
+
+  // it('retorna status 200 ao receber dados válidos', async () => {
+  //   // sinon.stub()
+  //   const result = await chai.request(app).post('/login').send({ ...userData })
+  //   expect(result.status).to.be.equal(200);
+  //   // expect(result.).to.be.equal('ok');
+  // });
+
+// });
